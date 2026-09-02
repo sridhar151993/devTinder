8c6b579015fb0f5c63050fed42f7dfb3040ae98c
sridhar p <sridhar.1593@gmail.com>
2026-09-02 15:42:27 +0530

    validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,65 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address");
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (value.length < 8) {
+                throw new Error("Password must be at least 8 characters long");
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        enum: ['Male', 'Female', 'Other']
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.pngall.com/profile-png/",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid URL");
+            }
+            
+        }
+    },
+    about: {
+        type: String,
+        maxlength: 500,
+        default: "This is a default about me section. You can update it later."
+    },
+    skills: {
+        type: [String],
+        unique: true
     }
-});
+},{timestamps: true});
 
 
 module.exports = mongoose.model("User", userSchema);
--- src/server.js
+++ src/server.js
@@ -8,16 +8,79 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
     // const { firstName, lastName, emailId, age, gender } = req.body
-
     const user = new User(req.body);
 
     try{
         await user.save();
         res.status(201).json("user saved successfully");
     } catch (error) {
-        res.status(500).json({ error: "Failed to save user" }); 
+        res.status(400).json({ error: error }); 
     }
 
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch users" });
+    }
+});
+
+
+app.delete("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        await User.findByIdAndDelete(userId);
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to delete user" });
+    }
+});
+
+
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    const updatedData = req.body;
+    
+    const allowedUpdates = ["password", "age", "gender", "photoUrl", "about", "skills"];
+
+    const isValidUpdate = Object.keys(updatedData).every((key) => allowedUpdates.includes(key));
+    if (!isValidUpdate) {
+        return res.status(400).json({ error: "Invalid update fields" });
+    }
+    
+    if (updatedData.skills) {
+
+    // Maximum 10 skills
+    if (updatedData.skills.length > 10) {
+        return res.status(400).json({
+            error: "Skills array cannot exceed 10 skills"
+        });
+    }
+
+    // Check duplicate skills
+    const uniqueSkills = new Set(updatedData.skills);
+
+    if (uniqueSkills.size !== updatedData.skills.length) {
+        return res.status(400).json({
+            error: "Skills cannot contain duplicates"
+        });
+    }
+}
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(userId, updatedData, 
+            {returnDocument : "after",runValidators:true });
+        if (!updatedUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json("User updated successfully");
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 connectDB().then(() => {
